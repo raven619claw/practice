81ead82d348bfce906207f96afcab0905433da28
Akash Sharma <akash.sharma1@proptiger.com>
2017-01-29 02:18:01 +0530

    adidng first assignment

--- src/public/scripts/modules/inmobi.js
+++ src/public/scripts/modules/inmobi.js
@@ -4,53 +4,70 @@ Application.addModule('inmobi', function(context) {
     var $pageElement;
     var timeoutId = 0;
     function init() {
-        // logger.info('initialization browsertack module');
         console.log('inmobi init');
         var moduleEl;
         moduleEl = context.getElement();
         $pageElement = $('#inmobiPageWrapper');
-        // window.addEventListener('scroll', function(){
-        //     onScrollheader();
-        // }, false);
-        (function() {
-            $(".main").UIPageScrolling({
-                sectionsControl: ".control-indicators",
-                captureTouch: true
-            });
-            $('.main').bind("touchstart", function() {
-                clearTimeout(timeoutId);
-                $(".page-control").css({ "opacity": "0.7" });
-            });
-            $('.main').bind("touchend", function() {
-                timeoutId = setTimeout(function() { $(".page-control").css({ "opacity": "0.1" }); }, 2000);
-            });
-            $('.main').bind("mousedown", function() {
-                timeoutId = setTimeout(function() { $(".page-control").css({ "opacity": "0.1" }); }, 2000);
-            });
-            $('.main').bind("mouseup", function() {
-                clearTimeout(timeoutId);
-                $(".page-control").css({ "opacity": "0.7" });
-            });
 
-            $(window).scroll(function(){
-                var $headerSelector = $pageElement.find('#header-v4');
-                var $scrollingSectionContainer = $pageElement.find('.ui-page-scrolling-main');
-                var $activeSectionSelector = $scrollingSectionContainer.find('.ui-page-scrolling-section.ui-page-scrolling-section_active');
-                if(!$activeSectionSelector.data('index')==0){
-                    $headerSelector.addClass('floating');
-                }else{
-                    $headerSelector.removeClass('floating');
-                }  
-            });
-        }());
+        $(".main").UIPageScrolling({
+            sectionsControl: ".control-indicators",
+            captureTouch: true
+        });
+
+        $(".main").bind("mousemove", function() {
+            headerEvents();
+        });
+
+        $(".main").bind("mousemove", function() {
+            headerEvents();
+        });
+        $(".main").bind("keydown", function() {
+            headerEvents();
+        });
+        $(".main").bind("keyup", function() {
+            headerEvents();
+        });
+        $(".main").bind("touchmove", function() {
+            headerEvents();
+        });
+        $(".main").bind('mousewheel', function() {
+            headerEvents();
+        });
+        $('.main').bind("touchstart", function() {
+            clearTimeout(timeoutId);
+            $(".page-control").css({ "opacity": "0.7" });
+            headerEvents();
+        });
+        $('.main').bind("touchend", function() {
+            timeoutId = setTimeout(function() { $(".page-control").css({ "opacity": "0.1" }); }, 2000);
+            headerEvents();
+        });
+        $('.main').bind("mousedown", function() {
+            timeoutId = setTimeout(function() { $(".page-control").css({ "opacity": "0.1" }); }, 2000);
+            headerEvents();
+        });
+        $('.main').bind("mouseup", function() {
+            clearTimeout(timeoutId);
+            $(".page-control").css({ "opacity": "0.7" });
+            headerEvents();
+        });
     }
 
+    function headerEvents() {
+        var $headerSelector = $('#header-v4');
+        var $scrollingSectionContainer = $('.ui-page-scrolling-main');
+        var $activeSectionSelector = $scrollingSectionContainer.find('.ui-page-scrolling-section.ui-page-scrolling-section_active');
+        if($activeSectionSelector.data('index')==0) {
+            $headerSelector.removeClass('floating');
+        }else{
+            $headerSelector.addClass('floating');
+        }
+    }
 
-    // function onScrollheader(){
-      
-    // }
     return {
         init
     };
 
 });
+
+
