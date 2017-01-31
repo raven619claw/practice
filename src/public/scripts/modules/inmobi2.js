946a9d073d8fc624c27749de3cf45cd59b24f08b
Akash Sharma <akash.sharma1@proptiger.com>
2017-02-01 02:27:11 +0530

    adding second page

--- src/public/scripts/modules/inmobi2.js
+++ src/public/scripts/modules/inmobi2.js
@@ -9,6 +9,19 @@ Application.addModule('inmobiTwo', function(context) {
         moduleEl = context.getElement();
         $pageElement = $('#inmobiTwoPageWrapper');
         scrollEvents();
+        
+        $('#nav-icon4').click(function(){
+            $(this).toggleClass('open');
+            $('body').toggleClass('open');
+        });
+
+        $('.blackBG').click(function(){
+            $('body').removeClass('open');
+            $('#nav-icon4').removeClass('open');
+        });
+        $('.hl').click(function(){
+            $(this).toggleClass('open');
+        })
     }
     function scrollEvents(){
         $(window).on('scroll', function(){
